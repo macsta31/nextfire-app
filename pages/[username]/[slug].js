a9fcea0c7f4b50632ded2c03c645857bbfec28f2
macsta31 <mackstathis@gmail.com>
2022-05-24 11:43:43 -0400

    end

--- pages/[username]/[slug].js
+++ pages/[username]/[slug].js
@@ -2,6 +2,9 @@ import styles from '../../styles/Post.module.css';
 import PostContent from '../../components/PostContent';
 import { firestore, getUserWithUsername, postToJSON } from '../../lib/firebase';
 import { useDocumentData } from 'react-firebase-hooks/firestore';
+import AuthCheck from '../../components/AuthCheck';
+import HeartButton from '../../components/HeartButton';
+import Link from 'next/link';
 
 
 export async function getStaticProps({ params }) {
@@ -66,6 +69,14 @@ export default function Post(props) {
           <strong>{post.heartCount || 0} 🤍</strong>
         </p>
 
+        <AuthCheck
+        fallback={
+          <Link href="/enter">
+            <button>💗 Sign Up</button>
+          </Link>
+        }>
+          <HeartButton postRef={postRef} />
+        </AuthCheck>
       </aside>
     </main>
   );
